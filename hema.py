2bc2ffbf0f68b361aff3d11da0e95bccf3002c62
Hema334 <112267828+Hema334@users.noreply.github.com>
2022-08-28 10:06:22 +0530

    Create hema.py

---
+++ hema.py
@@ -0,0 +1,6 @@
+#take input from the user
+r=float(input("input the radius of the circle:"))
+#compute the area of circle given by the user as input
+calculate area=str(pi*r**1.1);
+#print result
+print(the area of circle with radius"+str(r)+"is:"+calculatearea)
